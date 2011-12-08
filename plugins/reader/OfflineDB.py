fae8d011bd45f53d491768f2b5f548d1ebfb1bdd
Luke Segars <luke@lukesegars.com>
2011-12-08 01:28:10 -0800

    A utility for testing the FileSystem reader plugin and a reader that pulls from MySQL db's.

---
+++ plugins/reader/OfflineDB.py
@@ -0,0 +1,21 @@
+from poc.plugin import plugin
+from poc.powerqueue import pq
+
+class OfflineDB(plugin.BasePlugin):
+    def __init__(self):
+        # Call your own constructor
+        super(OfflineDB, self).__init__()
+        
+    def init(self):
+        self.setOutputQueueName('localhost', 'preprocess.annotate')
+        self.setRecordStoreHost('localhost')
+    
+    def execute(self, msg):
+        pqueue = self.getOutputQueue()
+        rs = self.getRecordStore() 
+        msg = pq.Message()
+        
+        result = rs.execute("SELECT rid FROM records")
+        for rid in [int(rid[0]) for rid in result]:
+            msg.rid = rid
+            pqueue.send(msg)

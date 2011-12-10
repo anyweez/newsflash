2e6700d91cf3a3fc04671af66aa714a4a7635421
Luke Segars <luke@lukesegars.com>
2011-12-09 19:20:55 -0800

    Still on the way.

--- setup-newsflash.sh
+++ setup-newsflash.sh
@@ -6,9 +6,8 @@ sudo apt-get -y install python-mysqldb python-feedparser python-gevent python-be
 sudo pip install pika==0.9.5
 sudo pip install pycassa
 
-wget http://10.240.107.17/photos.tar.gz
 mkdir -p ~/newsflash/data
 cd ~/newsflash/data
-mv photos.tar.gz data/
+wget http://10.240.107.17/photos.tar.gz
 tar zfx photos.tar.gz
 rm photos.tar.gz

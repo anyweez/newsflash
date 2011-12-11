3a8f900f2307e80f8a88102fad584ebb43faac58
Drew Fisher <drew.m.fisher@gmail.com>
2011-12-11 15:24:41 -0800

    Clean up queue purging.
    Signed-off-by: Drew Fisher <drew.m.fisher@gmail.com>

--- benchmark/launch_all_rabbit.sh
+++ benchmark/launch_all_rabbit.sh
@@ -20,18 +20,11 @@ echo "DROP TABLE IF EXISTS image" | mysql -u newsflash --password=rDZtewnGUULH2J
 # exist before deleting them (since amq-declare-queue IS idempotent)
 # This is somewhat ridiculous.
 echo "purging AMQP queues"
-amqp-declare-queue -d -q preprocess.crawl
-amqp-declare-queue -d -q preprocess.annotate
-amqp-declare-queue -d -q preprocess.similarity
-amqp-declare-queue -d -q preprocess.completed
-amqp-delete-queue -q preprocess.crawl
-amqp-delete-queue -q preprocess.annotate
-amqp-delete-queue -q preprocess.similarity
-amqp-delete-queue -q preprocess.completed
-amqp-declare-queue -d -q preprocess.crawl
-amqp-declare-queue -d -q preprocess.annotate
-amqp-declare-queue -d -q preprocess.similarity
-amqp-declare-queue -d -q preprocess.completed
+for QUEUE in preprocess.crawl preprocess.annotate preprocess.similarity preprocess.completed ; do
+	amqp-declare-queue -d -q ${QUEUE}
+	amqp-delete-queue -q ${QUEUE}
+	amqp-declare-queue -d -q ${QUEUE}
+done
 
 # Start the queue length watcher in the background
 echo "Starting queue length watcher..."

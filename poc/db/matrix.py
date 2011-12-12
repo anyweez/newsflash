b51a9a0d13f1e735e8c61ce313fd31b78688ec17
Luke Segars <luke@lukesegars.com>
2011-12-12 00:24:25 -0800

    Fixed (?) an error Thrift was encountering.

--- poc/db/matrix.py
+++ poc/db/matrix.py
@@ -61,15 +61,15 @@ class CassandraMatrixStore(object):
         self.pool_cf = pycassa.ColumnFamily(self.db_pool, self.table_name)
         
     def set_val(self, x, y, val):
-        self.pool_cf.insert(x, { str(y) : str(val) })
-        self.pool_cf.insert(y, { str(x) : str(val) })
+        self.pool_cf.insert(str(x), { str(y) : str(val) })
+        self.pool_cf.insert(str(y), { str(x) : str(val) })
     
     def get_val(self, x, y):
-        result = self.pool_cf.get(int(x), int(y))
+        result = self.pool_cf.get(str(x), int(y))
         return float(result[x])
     
     def getrow(self, x):
-        result = self.pool_cf.get(int(x))
+        result = self.pool_cf.get(str(x))
         output = {}
         for key, value in result:
             output[int(key)] = float(value)

e374f65e71bcdcd9c5eb67103a4b697ce7c0afa7
Drew Fisher <drew.m.fisher@gmail.com>
2011-12-11 16:19:52 -0800

    Change benchmark workflow.
    We attached the workers to queues...then deleted the queues to empty
    them.
    
    Instead:
    1) run wipe-rabbit.sh on rabbithole
    2) run launch_all_worker.sh on all the workers
    3) run launch_all_rabbit.sh on rabbithole
    4) ???
    5) PROFIT
    
    Signed-off-by: Drew Fisher <drew.m.fisher@gmail.com>

--- benchmark/launch_all_rabbit.sh
+++ benchmark/launch_all_rabbit.sh
@@ -11,21 +11,6 @@ echo "nuking logs/ directory..."
 rm -rf logs
 mkdir logs
 
-# Drop table from MySQL.
-echo "dropping image table"
-echo "DROP TABLE IF EXISTS image" | mysql -u newsflash --password=rDZtewnGUULH2Jjs newsflash
-
-# Purge the queues by deleting them and creating empty ones.
-# Since deleting a nonexistent queue will error, we ensure that all queues
-# exist before deleting them (since amq-declare-queue IS idempotent)
-# This is somewhat ridiculous.
-echo "purging AMQP queues"
-for QUEUE in preprocess.crawl preprocess.annotate preprocess.similarity preprocess.completed ; do
-	amqp-declare-queue -d -q ${QUEUE}
-	amqp-delete-queue -q ${QUEUE}
-	amqp-declare-queue -d -q ${QUEUE}
-done
-
 # Start the queue length watcher in the background
 echo "Starting queue length watcher..."
 python benchmark/get_queue_length.py preprocess.crawl preprocess.annotate preprocess.similarity preprocess.completed > logs/QueueLengthMonitor.log &
